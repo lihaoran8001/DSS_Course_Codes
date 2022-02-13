da38cc828e2f6152f743b7c61a1a63530fc1e9a9
lihaoran8001 <42938579+lihaoran8001@users.noreply.github.com>
2022-02-13 17:39:54 +0100

    Finish CH4 13

--- CH4/E4_13.R
+++ CH4/E4_13.R
@@ -1,16 +1,78 @@
-Weekly <- read.csv("./data/Weekly.csv")
+library(ISLR2)
+summary(Weekly)
 Weekly = na.omit(Weekly)
-Weekly[,9] = as.numeric(factor(Weekly[,9]))  # turn direction into numeric
+
 # (a)
 pairs(Weekly)
-cor(Weekly)  # show the correlation among different vars
+cor(Weekly[, -9])  # show the correlation among different vars
 # (b)
 attach(Weekly)
-lm.fit1 = lm(Direction~Lag1+Lag2+Lag3+Lag4+Lag5+Volume)
-summary(lm.fit1)
+glm.fit = glm(Direction~Lag1+Lag2+Lag3+Lag4+Lag5+Volume, data = Weekly, 
+               family = binomial)
+summary(glm.fit)
+# (c)
+glm.probs = predict(glm.fit, type = "response")
+glm.pred = rep("Down", length(glm.probs))
+glm.pred[glm.probs > 0.5] = "Up"
+table(glm.pred, Direction)
+# (d)
+train = (Year < 2009)
+Weekly.0910 = Weekly[!train, ]
+glm.fit = glm(Direction ~ Lag2, data = Weekly, family = binomial, subset = train)
+glm.probs = predict(glm.fit, Weekly.0910, type = "response")
+glm.pred = rep("Down", length(glm.probs))
+glm.pred[glm.probs > 0.5] = "Up"
+Direction.0910 = Direction[!train]
+table(glm.pred, Direction.0910)
+# (e)
+library(MASS)
+lda.fit = lda(Direction ~ Lag2, data = Weekly, subset = train)
+lda.pred = predict(lda.fit, Weekly.0910)
+table(lda.pred$class, Direction.0910)
+# (f)
+qda.fit = qda(Direction ~ Lag2, data = Weekly, subset = train)
+qda.class = predict(qda.fit, Weekly.0910)$class
+table(qda.class, Direction.0910)
+# (g)
+library(class)
+train.X = as.matrix(Lag2[train])
+test.X = as.matrix(Lag2[!train])
+train.Direction = Direction[train]
+set.seed(1)
+knn.pred = knn(train.X, test.X, train.Direction, k = 1)
+table(knn.pred, Direction.0910)
+# (h)
+install.packages("e1071")
+library(e1071)
+set.seed(120)  # Setting Seed
+nb.fit = naiveBayes(Direction ~ Lag2, data = Weekly, subset = train)
+nb.pred = predict(nb.fit, Weekly.0910)
+table(nb.pred, Direction.0910)
+# (i)
+# Logistic regression with Lag2:Lag1
+glm.fit = glm(Direction ~ Lag2:Lag1, data = Weekly, family = binomial, subset = train)
+glm.probs = predict(glm.fit, Weekly.0910, type = "response")
+glm.pred = rep("Down", length(glm.probs))
+glm.pred[glm.probs > 0.5] = "Up"
+Direction.0910 = Direction[!train]
+table(glm.pred, Direction.0910)
 
+# LDA with Lag2 interaction with Lag1
+lda.fit = lda(Direction ~ Lag2:Lag1, data = Weekly, subset = train)
+lda.pred = predict(lda.fit, Weekly.0910)
+mean(lda.pred$class == Direction.0910)
 
+# QDA with sqrt(abs(Lag2))
+qda.fit = qda(Direction ~ Lag2 + sqrt(abs(Lag2)), data = Weekly, subset = train)
+qda.class = predict(qda.fit, Weekly.0910)$class
+table(qda.class, Direction.0910)
 
+# KNN k =10
+knn.pred = knn(train.X, test.X, train.Direction, k = 10)
+table(knn.pred, Direction.0910)
 
+# KNN k = 100
+knn.pred = knn(train.X, test.X, train.Direction, k = 100)
+table(knn.pred, Direction.0910)
 detach(Weekly)
 
