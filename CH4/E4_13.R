8b546a0f2c9b9d7e8a1348f8d3defbd3a8c4cce2
OskariKettu <99014312+OskariKettu@users.noreply.github.com>
2022-02-14 09:28:13 +0100

    Update E4_13.R
    Got the value change thing working

--- CH4/E4_13.R
+++ CH4/E4_13.R
@@ -1,12 +1,15 @@
 library(ISLR2)
 summary(Weekly)
 Weekly = na.omit(Weekly)
-
+Weekly2 <- subset(Weekly, select = -(Direction)) 
+temp <- as.integer(as.character(Weekly$Direction)=="Up")
+Weekly2 <- subset(Weekly, select = -(Direction))
+Weekly["Direction"] <- temp # changes the direction value to 1 and 0
 # (a)
 pairs(Weekly)
-cor(Weekly[, -9])  # show the correlation among different vars
+cor(Weekly)  # show the correlation among different vars
 # (b)
-attach(Weekly)
+attach(Weekly) 
 glm.fit = glm(Direction~Lag1+Lag2+Lag3+Lag4+Lag5+Volume, data = Weekly, 
                family = binomial)
 summary(glm.fit)
