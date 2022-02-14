25e9851510b0574b84b4a479c5579cd57d346678
Zernez <fernando.pannullo@yahoo.it>
2022-02-14 11:32:31 +0100

    Done exercise (a) and (b) on 5.4

--- CH5/E5_4.R
+++ CH5/E5_4.R
@@ -5,3 +5,15 @@ Weekly2 <- subset(Weekly, select = -(Direction))
 temp <- as.integer(as.character(Weekly$Direction)=="Up")
 Weekly2 <- subset(Weekly, select = -(Direction))
 Weekly["Direction"] <- temp # changes the direction value to 1 and
+set.seed(1)
+attach(Weekly)
+
+# (a)
+
+glm.fit = glm(Direction ~ Lag1 + Lag2, data = Weekly, family = binomial)
+summary(glm.fit)
+
+# (b)
+
+glm.fit = glm(Direction ~ Lag1 + Lag2, data = Weekly[-1, ], family = binomial)
+summary(glm.fit)
