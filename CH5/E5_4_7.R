fcf5edcd9529f63c26b3ac4355ca6a83f08b4d75
lihaoran8001 <42938579+lihaoran8001@users.noreply.github.com>
2022-02-16 17:25:54 +0100

    update exercise 4.7 in chapter 5

--- CH5/E5_4_7.R
+++ CH5/E5_4_7.R
@@ -9,11 +9,29 @@ set.seed(1)
 attach(Weekly)
 
 # (a)
-
 glm.fit = glm(Direction ~ Lag1 + Lag2, data = Weekly, family = binomial)
 summary(glm.fit)
 
 # (b)
-
 glm.fit = glm(Direction ~ Lag1 + Lag2, data = Weekly[-1, ], family = binomial)
 summary(glm.fit)
+
+# (c)
+predict(glm.fit, Weekly[1,], type="response")
+Weekly[1,9]
+
+# (d)
+n = nrow(Weekly)
+LOOCV.err = rep(0, n)
+for(i in 1:n){
+  glm.fit = glm(Direction ~ Lag1 + Lag2, data = Weekly[-i,], family = binomial)
+  p = predict(glm.fit, Weekly[i,], type="response")
+  if((p > 0.5 && Weekly[i,9] == 1) || (p <= 0.5 && Weekly[i,9] == 0)){
+    LOOCV.err[i] = 0
+  }else{
+    LOOCV.err[i] = 1
+  }
+}
+
+# (e)
+mean(LOOCV.err)
