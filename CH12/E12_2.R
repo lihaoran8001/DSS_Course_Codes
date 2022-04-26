4498aae1588ddd6a805930610308f83626cb090b
Zernez <fernando.pannullo@yahoo.it>
2022-04-26 09:21:17 +0200

    E12_2 inserted

--- CH12/E12_2.R
+++ CH12/E12_2.R
@@ -1,5 +1,16 @@
+#a
 d = as.dist(matrix(c(0, 0.3, 0.4, 0.7, 
                      0.3, 0, 0.5, 0.8,
                      0.4, 0.5, 0.0, 0.45,
                      0.7, 0.8, 0.45, 0.0), nrow=4))
-plot(hclust(d, method="complete"))
+clust= hclust(d, method="complete")
+plot(clust,xlab= "Clusters", ylab= "Distance between observation")
+
+#b
+
+clust= hclust(d, method="single")
+plot(clust,xlab= "Clusters", ylab= "Distance between observation")
+
+#e
+
+plot(hclust(d, method="complete"), labels=c(2,1,4,3), xlab= "Clusters", ylab= "Distance between observation")
