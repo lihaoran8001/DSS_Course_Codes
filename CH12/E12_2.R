8a933f2517ad09b36f05554ba386cad8c1bdfa8e
Zernez <fernando.pannullo@yahoo.it>
2022-04-26 09:20:00 +0200

    E12_2 inserted

---
+++ CH12/E12_2.R
@@ -0,0 +1,5 @@
+d = as.dist(matrix(c(0, 0.3, 0.4, 0.7, 
+                     0.3, 0, 0.5, 0.8,
+                     0.4, 0.5, 0.0, 0.45,
+                     0.7, 0.8, 0.45, 0.0), nrow=4))
+plot(hclust(d, method="complete"))
